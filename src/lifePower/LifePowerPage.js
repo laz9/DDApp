715ddc129b0d1e36a5a0f264c7757deb36d2eef9
laz9 <sy_181222@163.com>
2020-11-28 09:50:07 +0800

    人生电量

--- src/lifePower/LifePowerPage.js
+++ src/lifePower/LifePowerPage.js
@@ -1,20 +1,60 @@
-import React, { Component } from 'react';
-import { Text, View ,Button} from 'react-native';
+import React, {Component} from 'react';
+import {
+  StyleSheet, 
+  View
+} from 'react-native';
+
+import { HcdWaveView } from '../utils/HcdWaveView';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <HcdWaveView
+          surfaceWidth = {230} 
+          surfaceHeigth ={230}
+          powerPercent = {80}
+          type="dc"
+          style = {{backgroundColor:'#FF7800'}}></HcdWaveView>
+        {/* <HcdWaveView
+          surfaceWidth = {230} 
+          surfaceHeigth ={230}
+          powerPercent = {10}
+          type="ac"
+          style = {{backgroundColor:'#FF7800'}}></HcdWaveView> */}
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  }
+});
 
 
-export default class LifePowerPage extends Component  {
-    constructor(){
-      super();
-      this.state={
+
+// import React, { Component } from 'react';
+// import { Text, View ,Button} from 'react-native';
+
+
+// export default class LifePowerPage extends Component  {
+//     constructor(){
+//       super();
+//       this.state={
        
-      };
-    }
+//       };
+//     }
 
-    render(){
-        return (
-            <View style={{justifyContent:"space-around",flex:1}}>
-                <Text>习惯页</Text>
+//     render(){
+//         return (
+//             <View style={{justifyContent:"space-around",flex:1}}>
+//                 <Text>习惯页</Text>
                
-            </View>
-        );
-    }}
+//             </View>
+//         );
+//     }}
